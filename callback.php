5836ed6d1e857e9a2b458e0369ffb853d51ea23a
CowboySt <42294187+CowboySt@users.noreply.github.com>
2021-11-22 21:16:30 +0800

    Update callback.php

--- callback.php
+++ callback.php
@@ -2,26 +2,60 @@
 <html>
 <body>
 <?php
-//接收回调
+ //文件放在青龙容器callback文件夹内，新建
+//接受wxpusher回调
 $json = file_get_contents("php://input");
 $data = json_decode($json, true);
-//分割uid
-foreach($data as $value) {
-  if ($value[appId] == 14207){
-    //取出pt_pin
-    $file = file("/ql/db/env.db");
-    $jd_ck = $file[count($file)-1];
-    $a = substr($jd_ck,strpos($jd_ck,';pt_pin=')+8);
-    $cost = substr($a,0,strpos($a,';'));
-    //写入配置文件
-    $origin = json_decode(file_get_contents('/ql/scripts/ccwav_QLScript2/CK_WxPusherUid.json'), true);
-      $origin[] = array(
-        'pt_pin' => $cost,
-        'Uid' => $value['uid'],
-    );
-  $wx_json = json_encode($origin);
-  file_put_contents('/ql/scripts/ccwav_QLScript2/CK_WxPusherUid.json', $wx_json);
 
+foreach($data as $value) {
+  if ($value[appId] == 14200){ //修改这里为你的应用id
+    //调取登录的jdck
+    $file = file("/ql/db/env.db");//数据库路径
+    $created_num = 0;
+    $pt_pin = "null";
+    for($i=0;$i<count($file);$i++){
+      $created_head = substr($file[$i],strpos($file[$i],'created')+9);
+      $created_end = substr($created_head,0,strpos($created_head,','));
+      $created = (int)$created_end;
+      if ($created < $created_num) {
+      } else {
+        $created_num = $created;
+        $pt_pin_head = substr($file[$i],strpos($file[$i],';pt_pin=')+8);
+        $pt_pin = substr($pt_pin_head,0,strpos($pt_pin_head,';'));
+
+      }
+    }
+    //CK_WxPusherUid.json文件
+    $origin = json_decode(file_get_contents('/ql/scripts/ccwav_QLScript2/CK_WxPusherUid.json'), true);//wxck路径
+    
+    function deep_in_array($value, $array) {
+      foreach($array as $item) {
+        if(!is_array($item)) {
+            if ($item == $value) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        if(in_array($value, $item)) {
+            return true;
+        } else if(deep_in_array($value, $item)) {
+            return true;
+        }
+      }
+      return false;
+    }
+
+    if (deep_in_array($pt_pin, $origin) == 1) {
+      echo "exist";
+    } else {
+      $origin[] = array(                            
+        'pt_pin' => $pt_pin,                          
+        'Uid' => $value['uid'],
+      );
+      $wx_json = json_encode($origin);             
+      file_put_contents('/ql/scripts/ccwav_QLScript2/CK_WxPusherUid.json', $wx_json);
+    }
   }
 }
 
