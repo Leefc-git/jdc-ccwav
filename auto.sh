d9e2493d1158465b15fe72d6534533c737b2586b
Leefc-git <42294187+Leefc-git@users.noreply.github.com>
2021-11-23 21:05:15 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,7 +3,7 @@
 
 apk add php7-fpm php7-mcrypt php7-soap php7-openssl php7-gmp php7-pdo_odbc php7-json php7-dom php7-pdo php7-zip php7-mysqli php7-bcmath php7-gd php7-odbc php7-pdo_mysql php7-gettext php7-xmlreader php7-xmlrpc php7-bz2 php7-iconv php7-curl php7-ctype php7-redis
 sed -i -e 's/short_open_tag = Off/short_open_tag = On/' /etc/php7/php.ini
-sed -i -e 's/nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf/nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf && php-fpm7/' docker/docker-entrypoint.sh
+sed -i '/reload/a\php-fpm7' docker/docker-entrypoint.sh
 echo "php安装完成，开始克隆仓库"
 
 git clone https://github.com/Leefc-git/nvjdc-ccwav.git
